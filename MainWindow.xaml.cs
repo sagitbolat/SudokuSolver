9efec5e815b5fba2bdee26cf807d905014eb7c7f
Sagit Bolat <sagitbolat@gmail.com>
2022-05-04 20:47:56 -0700

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            Button[,] buttonArray = new Button[9,9];
+            Grid grid = PlayGrid;
+
+            Color buttonBGColor = new Color();
+            buttonBGColor.R = 221;
+            buttonBGColor.G = 221;
+            buttonBGColor.B = 221;
+            buttonBGColor.A = 225;
+            Brush buttonBGBrush = new SolidColorBrush(buttonBGColor);
+
+            Color buttonFGColor = new Color();
+            buttonFGColor.R = 34;
+            buttonFGColor.G = 40;
+            buttonFGColor.B = 49;
+            buttonFGColor.A = 225;
+            Brush buttonFGBrush = new SolidColorBrush(buttonFGColor);
+            //FontFamily buttonFont = new FontFamily(new Uri("pack://application:,,,"), "FuturaBook.ttf");
+
+            //iterate over the whole grid
+            for (int x = 0; x < 9; x++) {
+                for (int y = 0; y < 9; y++) {
+                    //create button
+                    Button button = new Button();
+
+                    //set button asthetics
+                    button.Background = buttonBGBrush;
+                    button.Foreground = buttonFGBrush;
+                    button.Content = "j";
+                    button.FontSize = 40;
+                    //button.FontFamily = buttonFont;
+
+                    //add button to array
+                    buttonArray[x, y] = button;
+                    
+                    // add the button to the grid control
+                    Grid.SetRow(button, y);
+                    Grid.SetColumn(button, x);
+                    grid.Children.Add(button);
+                }
+            }
+        }
+    }
+}
---
+++ SudokuGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver {
+    class SudokuGenerator {
+
+        //Generates a grid of integers;
+        public int[,] Generate() {
+            int[,] puzzleGrid = new int[9, 9];
+
+            //Generate a complete solution using backtracking that fills up the grid
+            puzzleGrid = GenerateCompleteSolution();
+            //Remove numbers from the grid, while making sure the solution is still unique.
+
+            return puzzleGrid;
+        }
+
+        private int[,] GenerateCompleteSolution() {
+            int[,] grid = new int[9, 9];
+
+            for (int i = 0; i < 81; i++) {
+                int row = i / 9;
+                int col = i % 9;
+                if (grid[row,col] == 0) {
+
+                }
+            }
+
+            return grid;
+        }
+    }
+}
